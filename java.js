381b0b78abe458565861266746a019666e652b16
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-11-16 20:49:05 -0500

    restructuring website

--- java.js
+++ java.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
   $('.parallax').parallax();
-  $('.carousel').carousel();
   $('.sidenav').sidenav();
   $('select').formSelect();
-  autoplay();
 });
-
-function autoplay() {
-  $('.carousel').carousel('next');
-  setTimeout(autoplay, 4500);
-}
